f908037ee251151deed6c223c819c2a532ccd8a7
Mykola <nbobos@gmail.com>
2023-09-28 19:47:45 +0300

    init

---
+++ src/QALight/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package QALight;
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Слава Україні!");
+	}
+
+}
